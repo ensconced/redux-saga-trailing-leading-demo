290a9fe0eca946ad93251d9241719c029353c87a
Joe Barnett <joebarnett876@gmail.com>
2023-06-01 21:32:11 +0100

    set up test harness

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
+import { debounce, put, throttle } from "redux-saga/effects";
 import createSagaMiddleware from "redux-saga";
 import { createStore, applyMiddleware } from "redux";
 
-function* lineSaga() {}
+function* addLine(divId) {
+  yield put({ type: "add-line", divId });
+}
+
+function* lineSaga() {
+  yield debounce(200, "mousemove", addLine, "debounce");
+  yield throttle(200, "mousemove", addLine, "throttle");
+}
+
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-  (invocationCount = 0, action) => {
+  (state = { debounce: 0, throttle: 0 }, action) => {
     if (action.type === "add-line") {
-      return invocationCount + 1;
+      return {
+        ...state,
+        [action.divId]: (state[action.divId] + 1) % 1000,
+      };
+    } else {
+      return state;
     }
   },
-  0,
+  { debounce: 0, throttle: 0 },
   applyMiddleware(sagaMiddleware)
 );
+
 store.subscribe((...args) => {
-  console.log(store.getState());
+  ["debounce", "throttle"].forEach((divId) => {
+    const count = store.getState()[divId];
+    document.getElementById(divId).innerHTML = "█".repeat(count);
+  });
 });
 
 sagaMiddleware.run(lineSaga);
 
-setInterval(() => {
-  store.dispatch({ type: "add-line" });
-}, 1000);
+document.addEventListener("mousemove", () => {
+  store.dispatch({ type: "mousemove" });
+});
